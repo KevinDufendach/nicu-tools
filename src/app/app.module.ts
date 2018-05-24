cee83a00ab6ddf065bd8723a477ea707b379d5ed
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2018-05-24 16:02:32 -0400

    create material front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,38 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { RouterModule, Routes } from '@angular/router';
+import { PreEclampsiaCalcComponent } from './pre-eclampsia-calc/pre-eclampsia-calc.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+
+const appRoutes: Routes = [
+  {path: 'pre-e', component: PreEclampsiaCalcComponent},
+  {path: '', redirectTo: '/pre-e', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreEclampsiaCalcComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
